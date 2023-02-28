1e8e995ac19174aa848570391eac3b927b2b5031
Mike Pollard <1465638+MikeDP@users.noreply.github.com>
2023-02-28 14:33:15 +0000

    ASCON authentication/encryption
    Using ASCON1.2 authenticated encryption for UDP packets

--- udp_listener.py
+++ udp_listener.py
@@ -1,25 +1,23 @@
 #! /usr/bin/env python3
-
 """
 This applet is a minimal framework example of a UDP listener.
-It uses separate threads and queues to 
-  * receive and decode udp packet and write to receeve_Q
+It uses separate threads and queues to
+  * receive and decode udp packet and write to receive_Q
   * pull packets from receive_Q and handle them
   * place any respnse on response_Q
   * pull responses from response_Q, encode and transmit back to client
-
 """
 
 import hashlib
 import json
 import os
 import socket
-import struct
 import threading
 from datetime import datetime
 from pathlib import Path
 from queue import Queue
 
+from ascon import ascon
 from dateutil import parser
 
 from log2d import Log, logging
@@ -27,12 +25,12 @@ from log2d import Log, logging
 # and this ~may~ not be the PIP package yet!  
 # See https://github.com/PFython/log2d
 
-__VER__ = 'udp_listner v0.2' # Version string
+__VER__ = 'udp_listener v0.3' # Version string
 
 # ############################ CONSTANTS ########################
 LOGBASE = Path(os.environ['HOME'], '.logs')   # Base folder for logs
 UDP_SRC = ('<broadcast>', 6666)   # UDP listener address:port
-SECRET = "Your5ecret"   # SALT for haslib
+SECRET = "M15ecret"   # key for encryption
 LOGSIZE = 1024 * 1024  # 1MB before log rotate
 LOGGERS = {}   # Dict of active loggers
 
@@ -40,43 +38,57 @@ LOGGERS = {}   # Dict of active loggers
 
 full_path = lambda fn: Path(LOGBASE, fn +'.log')
 
-def json_encode(data: str, salt: str) -> bytes:
-    """Encode the 'data' into a bytes string prepending the length and salted SHA256 digest"""
-    hash = hashlib.sha256()
-    try:
-        # encode as JSON and convert to bytes
-        json_text = json.dumps(data, default=str).encode('UTF-8')
-        # Salt the hash with the secret
-        hash.update(salt.encode('UTF-8'))
-        hash.update(json_text)
-        digest = hash.digest()
-        # Add the length of the data and the digest to the beginning of the bytes
-        json_bytes = struct.pack('!i', len(json_text+digest)) + digest + json_text
-    except Exception as excep:
-        print(f"Exception during makePickle: {excep}")
-        return None
-    return json_bytes
+def json_encode(data: any, key: str) -> bytes:
+    """Encode the 'data' into a bytes string using ASCON"""
+    # encode as JSON
+    json_data = json.dumps(data, default=str)
+    # encrypt with ASCON
+    encrypted_bytes = asc_encrypt(json_data, key).encode('utf-8')
+    return encrypted_bytes
 
-def json_decode(data, salt:str):
+def json_decode(data:bytes, key:str) -> any:
     """Unpack the data packet"""
-    # Extract the length from the beginning of the packet
-    length = struct.unpack('!i', data[:4])[0] + 4
-    # Check the length of the data
-    if len(data) != length:
-        raise ValueError("Data length check failed")
-    # Get the digest
-    digest = data[4:4+hashlib.sha256().digest_size]
-    # Extract the json data from the packet
-    json_bytes = data[4+hashlib.sha256().digest_size:]
-    # Calculate the digest of the json data
-    calculated_digest = hashlib.sha256(salt.encode('utf-8')+json_bytes).digest()
-    # Compare the calculated digest with the one received in the packet
-    if calculated_digest != digest:
-        raise ValueError("Data integrity check failed")
-    # Convert the json bytes to a dictionary
-    json_data = json_bytes.decode()
-    log_dict = json.loads(json_data)
-    return log_dict
+    # Decrypt with ASCON
+    json_data = asc_decrypt(data.decode('utf-8'), key)
+    # and decode from jason
+    decrypt = json.loads(json_data)
+    return decrypt
+
+def asc_encrypt(plaintext:str, key:str) -> str:
+    """ASCON1.2 encryption.  Plaintext: str, key: str will hashed to 16 bytes.
+        Returns: string of hex characters
+    """
+    # Fix key and convert to bytes
+    _key = hashlib.md5(key.encode('utf-8')).digest()
+    #Convert plaintext to byte
+    plaintext_bytes = plaintext.encode('utf-8')
+    # Generate a random 16 byte nonce
+    nonce = ascon.get_random_bytes(16)
+    # Encrypt plaintext with key and nonce
+    ciphertext = ascon.ascon_encrypt(_key, nonce, b"", plaintext_bytes)
+    # Append the nonce to the ciphertext
+    encrypted_message = nonce + ciphertext
+    # Convert encrypted message bytes to string of hex
+    return encrypted_message.hex()  # string of hex characters
+
+def asc_decrypt(encrypted_message_hex:str, key:str) -> str:
+    """ASCON1.2 decryption. encrypted_message_hex: str, key: str will hashed to 16 bytes.
+        Returns: str
+    """
+    # Fix key to 16 bytes
+    _key = hashlib.md5(key.encode('utf-8')).digest()
+    # Convert encrypted message from hex to bytes
+    encrypted_message = bytes.fromhex(encrypted_message_hex)
+    # Extract the nonce and ciphertext from the encrypted message
+    nonce = encrypted_message[:16]
+    ciphertext = encrypted_message[16:]
+    # Decrypt ciphertext with key and nonce
+    try:
+        plaintext_bytes = ascon.ascon_decrypt(_key, nonce, b"", ciphertext)
+    except ValueError:
+        raise ValueError("Decryption failed - message has been tampered with")
+    # Convert plaintext bytes to string
+    return plaintext_bytes.decode()  # String
 
 def udp_listener(queue, secret):
     """Thread to continually listen for UDP messages.  These are 'unpacked'
@@ -91,11 +103,7 @@ def udp_listener(queue, secret):
     while True:
         # Receive data from the socket
         data, address = sock.recvfrom(4096)
-        # Unpack it
-        message_data = json_decode(data, secret)
-        if message_data:
-            # Put the received data and address into the queue as tuple
-            queue.put((message_data, address))
+        queue.put((data, address))   # put raw message on Q
 
 def message_handler(queue, response_queue):
     """Handle messages.  Read message from the receive queue passes it to
@@ -103,10 +111,11 @@ def message_handler(queue, response_queue):
     """
     while True:
         # Get data from the queue
-        data, address = queue.get()
+        encrypted_msg, address = queue.get()
 
         # Start message handler thread
-        message_thread = threading.Thread(target=handle_message, args=(data, address, response_queue,))
+        message_thread = threading.Thread(target=handle_message,
+                                args=(encrypted_msg, address, response_queue))
         message_thread.start()
 
 def get_logger(log_name):
@@ -124,10 +133,12 @@ def get_logger(log_name):
         LOGGERS[log_name] = logger
     return LOGGERS[log_name]  # retrieve the logger from the loggers dict
 
-def handle_message(msg, address, response_queue):
-    """Handle the decoded message:  Log any log messages and respond
+def handle_message(encrypted_msg, address, response_queue):
+    """Handle the message:  Log any log messages and respond
        to any other handlable requests
     """
+    # decrypt message here
+    msg = json_decode(encrypted_msg, SECRET)
     if not isinstance(msg, dict):
         print(f"msg is not a dict: {msg}")
         return
@@ -149,7 +160,7 @@ def handle_message(msg, address, response_queue):
         logger.handle(log_record)
         if msg['levelno'] >= 50:   # This is critical or above
             ... # send suitable email alert
-            # or alternatively add SMTPHandler
+            # or add SMTPHandler also
 
     elif _cmd == "FIND":   # Search a log
         # get params
@@ -181,7 +192,6 @@ def handle_message(msg, address, response_queue):
     elif _cmd == "VER":
         # Respond with UDPLogger version.  This can be used to show its working
         response_queue.put((__VER__, address))
-
 
 def response_sender(response_queuelogger):
     """Sends data (str, list, tuple, number) to addr through sock via UDP"""
